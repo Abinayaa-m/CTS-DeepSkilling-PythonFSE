b9899276b493e47750d1b1c6e515887b93d5c1f0
abinaya <you@example.com>
2026-06-25 07:50:29 +0530

    DB Module completed

---
+++ Module3_DatabaseIntegration/Abinaya_M/HandsOn_7/models.py
@@ -0,0 +1,99 @@
+from sqlalchemy import (
+    DECIMAL,
+    Column,
+    Date,
+    Integer,
+    String,
+    ForeignKey,
+    Time,
+    create_engine
+)
+from sqlalchemy import Boolean
+
+from sqlalchemy.orm import (
+    declarative_base,
+    relationship
+)
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+Base = declarative_base()
+
+engine = create_engine(
+    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}",
+    echo=True
+)
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    department_id = Column(Integer, primary_key=True, autoincrement=True)
+    dept_name = Column(String(100), nullable=False)
+    head_of_dept = Column(String(100))
+    budget = Column(DECIMAL(12,2))
+
+class Student(Base):
+    __tablename__ = "students"
+
+    student_id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    date_of_birth = Column(Date)
+    department_id = Column(Integer, ForeignKey("departments.department_id"))
+    enrollment_year = Column(Integer)
+    department = relationship("Department")
+    is_active = Column(Boolean, default=True) #Q98 ADDED is_active to alter the table and add a new column to the students table
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    course_id = Column(Integer, primary_key=True, autoincrement=True)
+    course_name = Column(String(150), nullable=False)
+    course_code = Column(String(20))
+    credits = Column(Integer)
+    department_id = Column(Integer,ForeignKey("departments.department_id"))
+
+class Enrollment(Base):
+    __tablename__ = "enrollments"
+
+    enrollment_id = Column(Integer, primary_key=True, autoincrement=True)
+    student_id = Column(Integer, ForeignKey("students.student_id"))
+    course_id = Column(Integer,ForeignKey("courses.course_id"))
+    enrollment_date = Column(Date)
+    grade = Column(String(2))
+    student = relationship("Student")
+    course = relationship("Course")
+
+class Professor(Base):
+    __tablename__ = "professors"
+
+    professor_id = Column(Integer, primary_key=True, autoincrement=True)
+    prof_name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True)
+    department_id = Column(Integer, ForeignKey("departments.department_id"))
+    salary = Column(DECIMAL(10,2))
+
+#added CourseSchedule model to create a new table course_schedules with the specified columns and relationships.(Q102)
+class CourseSchedule(Base):
+    __tablename__ = "course_schedules"
+    schedule_id = Column(Integer, primary_key=True)
+    course_id = Column(Integer, ForeignKey("courses.course_id"))
+    day_of_week = Column(String(20))
+    start_time = Column(Time)
+    end_time = Column(Time)
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    print("Tables created successfully")
+
+# Base.metadata.create_all(engine) automatically
+# creates all tables defined by ORM model classes.
+# Verified in MySQL Workbench using SHOW TABLES.
